a039c914484789beb32c6c33d5f8d136e65479df
AlexSth <alexshrestha77@gmail.com>
2020-10-01 15:18:11 +0545

    Oauth implemented

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
-use Laravel\Passport\Passport;
+
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -36,7 +36,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Passport::routes();
+
         $this->configureRateLimiting();
 
         $this->routes(function () {
